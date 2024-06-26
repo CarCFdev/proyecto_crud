063638179dc77d5396df9fe59da630bae29d3f2a
CarCfdev <user.email>
2024-06-26 17:51:00 -0300

    agrego la verificacion de la bd

--- database/connection.js
+++ database/connection.js
@@ -1,36 +1,24 @@
-import pkg from 'pg';
-const { Pool } = pkg;
+import pg from 'pg';
 
 // Configuración de la conexión a la base de datos en Render
-const pool = new Pool({
+const pool = new pg.Pool({
     connectionString: 'postgresql://carpw:dm01T4PiP9N4naBSKoiGmlt5aSRemfWE@dpg-cpiu6k21hbls73bn8d1g-a.oregon-postgres.render.com/dbapp_i2oj',
     ssl: {
-        rejectUnauthorized: true // Necesario si Render requiere SSL y no tienes certificados configurados
+        rejectUnauthorized: true // Ajusta esto según los requisitos de tu proveedor de base de datos
     }
 });
 
-// Crear la tabla 'feriados' al iniciar la aplicación si no existe
-async function crearTablaFeriados() {
-    try {
-        const createTableQuery = `
-            CREATE TABLE IF NOT EXISTS feriados (
-                id SERIAL PRIMARY KEY,
-                nombre VARCHAR(255) NOT NULL,
-                fecha DATE NOT NULL
-            )
-        `;
-        await pool.query(createTableQuery);
-        console.log('Tabla "feriados" creada correctamente o ya existente.');
-    } catch (error) {
-        console.error('Error al crear la tabla "feriados":', error);
+// Verificar la conexión inicial
+pool.connect((err, client, done) => {
+    if (err) {
+        console.error('Error al conectar a la base de datos:', err);
+    } else {
+        console.log('Conexión establecida con la base de datos PostgreSQL en Render');
+        done(); // Liberar el cliente de la conexión
     }
-}
-
-// Llamar a la función para crear la tabla al iniciar la aplicación
-crearTablaFeriados();
+});
 
 export default pool;
-
 
 
 /*import "dotenv/config";
